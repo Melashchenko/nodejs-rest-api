1094214944b48335465f1ecc3df70cd0c330a4ab
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-05-30 22:00:56 +0300

    finished hw03

---
+++ models/contact.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const contactAddSchema = Joi.object({
+  name: Joi.string()
+
+    .min(3)
+    .max(30)
+    .required()
+    .messages({ "any.required": `missing required name field` }),
+  email: Joi.string()
+
+    .required()
+    .messages({ "any.required": `missing required email field` }),
+  phone: Joi.string()
+    .required()
+
+    .messages({ "any.required": `missing required phone field` }),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+contactSchema.post("save", handleMongooseError);
+
+const Contact = model("contact", contactSchema);
+
+const schemas = {
+  contactAddSchema,
+  updateFavoriteSchema,
+};
+
+module.exports = {
+  Contact,
+  schemas,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,91 +1,34 @@
 const express = require("express");
-const Joi = require("joi");
 
-const contactsService = require("..//../models/contacts");
-const { HttpError } = require("..//../helpers");
+const contactsController = require("../../controllers/contacts-controller");
+const { schemas } = require("../../models/contact");
+const { validateBody } = require("../../decorators");
+const { isValidId } = require("../../middlewares");
+
 const router = express.Router();
 
-const contactAddSchema = Joi.object({
-  name: Joi.string()
-    .alphanum()
-    .min(3)
-    .max(30)
-    .required()
-    .messages({ "any.required": `missing required name field` }),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required()
-    .messages({ "any.required": `missing required email field` }),
-  phone: Joi.string()
-    .required()
-    .regex(/^[0-9]{10}$/)
-    .messages({ "any.required": `missing required phone field` }),
-});
+router.get("/", contactsController.getAllContacts);
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contactsService.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:contactId", isValidId, contactsController.getContactById);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  "/",
+  validateBody(schemas.contactAddSchema),
+  contactsController.addContact
+);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contactsService.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", contactsController.removeContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put(
+  "/:contactId",
+  validateBody(schemas.contactAddSchema),
+  contactsController.updateContact
+);
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing fields");
-    }
-    const result = await contactsService.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  "/:contactId/favorite",
+  validateBody(schemas.updateFavoriteSchema),
+  contactsController.updateFavoriteContact
+);
 
 module.exports = router;
