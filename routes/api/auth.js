afb2e087129dd90fb461803335ac6f1bd3f51e14
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-06-05 22:52:29 +0300

    done hw05

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,16 +3,23 @@ const express = require("express");
 const { schemas } = require("../../models/user");
 const { validateBody } = require("../../decorators");
 const ctrl = require("../../controllers/auth");
-const { authenticate } = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 
 const router = express.Router();
 
-router.post("/signUp", validateBody(schemas.singUpSchema), ctrl.singUp);
+router.post("/signUp", validateBody(schemas.singUpSchema), ctrl.signUp);
 
-router.post("/signIn", validateBody(schemas.signInSchema), ctrl.singIn);
+router.post("/signIn", validateBody(schemas.signInSchema), ctrl.signIn);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/signOut", authenticate, ctrl.signOut);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
